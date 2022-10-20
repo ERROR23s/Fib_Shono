3057e44c9a33cd0f807d945391598b2a6bf1607f
Pavel <Shonowin@gmail.com>
2022-10-20 21:58:27 +0300

    ggs

--- main.py
+++ main.py
@@ -1,5 +1,18 @@
 def main():
-  print('My first git program')
-  print("gg")
+    print("Введите требуемое число фибаначи")
+    Nfib = int(input())
+    print(Fibanachi(Nfib))
+
+def Fibanachi(value):
+    i=0
+    Ni=0
+    Ni_1 =1
+    while i<value:
+        Nvr = Ni
+        Ni +=Ni_1
+        Ni_1=Nvr
+        i+=1
+    return Ni
+
 if __name__ == '__main__':
- main()
+    main()
